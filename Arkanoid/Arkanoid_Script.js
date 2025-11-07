6c58e323e7da405ded1612d4e220d7d9335193ac
Luan Chaves <56284871+LuanChavesBermudez@users.noreply.github.com>
2025-11-06 18:41:06 -0600

    ball movement, level 1 generation

--- Arkanoid/Arkanoid_Script.js
+++ Arkanoid/Arkanoid_Script.js
@@ -1,3 +1,4 @@
+let gameInstance;
 const keyboardEvents = {};
 
 document.addEventListener("keydown", (event) => {
@@ -10,7 +11,7 @@ document.addEventListener("keyup", (event) => {
 
 class Paddle {
     constructor(areaWidth, areaHeight){
-        this.width = 150;
+        this.width = 125;
         this.height = 20;
         this.posX = areaWidth/2 - this.width/2;
         this.posY = areaHeight - 50;
@@ -34,6 +35,7 @@ class Paddle {
         context.strokeStyle = "black";
         context.lineWidth = 2;
         context.stroke();
+        context.closePath();
     }
 
     moveLeft(){
@@ -60,17 +62,123 @@ class Paddle {
     }
 }
 
-class Brick {}
+class Ball {
+    constructor(areaWidth, areaHeight) {
+        this.radius = 6;
+        this.posX = areaWidth / 2 - this.radius;
+        this.posY = areaHeight / 2;
+        this.horizontalSpeed = -6;
+        this.verticalSpeed = -6; 
+    }
+
+    /*
+    constructor(posX, posY, horizontalSpeed, verticalSpeed) {
+        this.radius = 10;
+        this.posX = posX;
+        this.posY = posY;
+        this.horizontalSpeed = horizontalSpeed;
+        this.verticalSpeed = verticalSpeed; 
+    }
+    */
+
+    render(context) {
+        context.fillStyle = 'white';
+        context.beginPath();
+        context.arc(this.posX, this.posY, this.radius, 0, Math.PI * 2);
+        context.fill();
+        context.strokeStyle = 'black';
+        context.lineWidth = 1;
+        context.stroke();
+        context.closePath();
+    }
+
+    move(areaWidth) {
+        if (this.posX == 0 || this.posX == areaWidth - this.radius) {
+            this.horizontalBounce();
+        }
+        if (this.posY == 0) {
+            this.verticalBounce();
+        }
+
+        if (this.posX + this.horizontalSpeed > areaWidth - this.radius) {
+            this.posX = areaWidth - this.radius;
+            return;
+        }
+
+        if (this.posX + this.horizontalSpeed < 0) {
+            this.posX = 0;
+            return;
+        }
+
+        if (this.posY + this.verticalSpeed < 0) {
+            this.posY = 0;
+            return;
+        }
+
+        this.posX += this.horizontalSpeed;
+        this.posY += this.verticalSpeed;
+    }
+
+    horizontalBounce() {
+        this.horizontalSpeed *= -1;
+    }
+
+    verticalBounce() {
+        this.verticalSpeed *= -1;
+    }
+}
+
+const BrickStatus = {
+    ACTIVE: true,
+    DESTROYED: false
+}
+
+class Brick {
+    constructor(width, height, posX, posY, colorStr) {
+        this.width = width;
+        this.height = height;
+        this.posX = posX;
+        this.posY = posY;
+        this.colorStr = colorStr;
+        this.status = BrickStatus.ACTIVE;
+    }
+
+    render(context) {
+        if (this.status) {
+            context.fillStyle = this.colorStr;
+            context.fillRect(this.posX, this.posY, this.width, this.height);
+            context.strokeStyle = "black";
+            context.lineWidth = 1;
+            context.strokeRect(this.posX, this.posY, this.width, this.height);
+        }
+    }
+
+    destroy() {
+        this.status = BrickStatus.DESTROYED;
+    }
+}
 
 class Game {
     constructor(gameArea) {
         this.gameArea = gameArea;
+        this.areaWidth = gameArea.width;
+        this.areaHeight = gameArea.height;
         this.context = gameArea.getContext("2d");
-        this.paddle = new Paddle(gameArea.width, gameArea.height);
+        this.initializeLogic();
+    }
+
+    initializeLogic() {
+        this.score = 0;
+        this.isPaused = false;
+        this.context.clearRect(0, 0, this.areaWidth, this.areaHeight);
+        this.paddle = new Paddle(this.areaWidth, this.areaHeight);
+        this.ball = new Ball(this.areaWidth, this.areaHeight);
+        this.brickMatrix = [];
     }
     
     gameLoop(){
-        this.context.clearRect(0, 0, this.gameArea.width, this.gameArea.height);
+        this.context.clearRect(0, 0, this.areaWidth, this.areaHeight);
+        this.score += 1;
 
         if (keyboardEvents["ArrowLeft"]) {
             this.movePaddleLeft();
@@ -78,13 +186,41 @@ class Game {
         if (keyboardEvents["ArrowRight"]) {
             this.movePaddleRight();
         }
+        this.ball.move(this.areaWidth);
 
         this.renderObjects();
-        requestAnimationFrame(this.gameLoop.bind(this));
+        if (!this.isPaused) {
+            requestAnimationFrame(this.gameLoop.bind(this));
+        }  
     }
 
     renderObjects() {
         this.paddle.render(this.context);
+        this.ball.render(this.context);
+        for (const row of this.brickMatrix){
+            for (const brick of row) {
+                brick.render(this.context);
+            }
+        }
+    }
+
+    togglePause(){
+        this.isPaused = !this.isPaused;
+        if (this.isPaused) {
+            cancelAnimationFrame(this.gameLoop);
+        } else {
+            this.gameLoop();
+        }
+    }
+
+    stop(){
+        this.isPaused = true;
+        cancelAnimationFrame(this.gameLoop);
+    }
+
+    restart() {
+        this.stop();
+        this.initializeLogic();
     }
 
     movePaddleLeft() {
@@ -94,10 +230,38 @@ class Game {
     movePaddleRight() {
         this.paddle.moveRight(this.gameArea.width);
     }
+
+    buildLevel1() {
+        const brickWidth = 56;
+        const brickHeight = 25;
+        const levelMargin = (this.gameArea.height / 2) - (brickHeight * 5);
+        for (let i = 0; i < 4; i++) {
+            this.brickMatrix[i] = [];
+            for (let j = 0; j < 12; j++) {
+                let colorStr = "white";
+                
+                switch (i) {
+                    case 0:
+                        colorStr = "skyblue";
+                        break;
+                    case 1:
+                        colorStr = "red";
+                        break;
+                    case 2:
+                        colorStr = "yellow";
+                        break;
+                    default:
+                        colorStr = "green";
+                }
+                this.brickMatrix[i][j] = new Brick(brickWidth, brickHeight, brickWidth * j, levelMargin - (brickHeight * i), colorStr);
+            }
+        }
+    }
 }
 
 window.onload = function(){
     const canvas = document.getElementById("ArkanoidCanvas");
     gameInstance = new Game(canvas);
+    gameInstance.buildLevel1();
     gameInstance.gameLoop();
 }
